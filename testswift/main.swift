e4f2bb88925c7e719d9a676830a7a6d36da75219
chenjingrong <chenjingrong@chenjingrongdeMacBook-Air.local>
2017-12-01 12:09:19 +0800

    team+1

--- testswift/main.swift
+++ testswift/main.swift
@@ -3,10 +3,24 @@
 //  testswift
 //
 //  Created by 陈景荣 on 15/3/30.
-//  Copyright (c) 2015年 陈景荣. All rights reserved.
 //
 
 import Foundation
+let label="the world is "
+let width=94
+let widthd=label+String(width)
+let indi=[75,39,34,33]
+var team=1
+for ac in indi{
+    if ac>50
+    {
+        team+=3
+    } else
+    {
+        team+=5
+    }
+}
+var car="aaaaa"
+print("car length:\(car.characters.count)")
 
-println("Hello, World!")
 
